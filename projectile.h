b7fcacd97872731fcc8e55bcc57044cdf911215a
ChrisFoster4 <chrismarkfoster@yahoo.co.uk>
2018-05-08 00:41:03 +0100

    Forgot to add header files.....

--- projectile.h
+++ projectile.h
@@ -1,14 +1,25 @@
-#include <math.h>
+#pragma once
 #include <unistd.h> //For sleep()
+#include <string>
+#include <iostream> //Required for cout
+#include <GL/glut.h> //apt install freeglut3-dev for Debian based systems
+
 class Projectile{
-    float x,y,diameter;
-public:
-    void setPosition(float x,float y,float radius);;
-    void movePosition(float x,float y,float radius);
-    void output();
-    Projectile duplicate(Projectile& pro);
-    float getDiameter();
-    float getX();
-    float getY();
-    std::string describe();
-} ;
+	private:
+		float x,y,diameter;
+	public:
+		Projectile();
+		Projectile(float startX,float startY,float startDiameter);
+		void output();
+		Projectile duplicate(Projectile& pro);
+
+		//Mutator methods
+		void movePosition(float x,float y,float radius);
+		void setPosition(float x,float y,float radius);;
+
+		//Accessor methods
+		float getDiameter();
+		float getX();
+		float getY();
+		std::string describe();
+};
